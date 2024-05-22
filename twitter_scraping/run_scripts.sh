9b94def387a245980d392f588cb2be342a213ea1
palistha.deshar <palistha.deshar@tai.com.np>
2024-05-22 11:35:54 +0545

    shell script modified

--- twitter_scraping/run_scripts.sh
+++ twitter_scraping/run_scripts.sh
@@ -11,7 +11,7 @@ year="$1"
 # Define paths to your Python scripts
 twitter_scraping="scripts/twitter_scraping.py"
 concat_csv="scripts/concat_csvs.py"
-check_complaint="scripts/check_complaint.py"
+check_complaint="scripts/check_complaints_llm.py"
 
 # Run each Python script with the specified year as an argument
 echo "Running scripts for year $year..."
