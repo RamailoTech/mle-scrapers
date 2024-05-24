17966b04fc37a43436edcaa1efda1dccd17b2f87
palistha.deshar <palistha.deshar@tai.com.np>
2024-05-24 21:03:56 +0545

    shell script modified

--- twitter_scraping/run_scripts.sh
+++ twitter_scraping/run_scripts.sh
@@ -10,16 +10,23 @@ year="$1"
 
 # Define paths to your Python scripts
 twitter_scraping="scripts/twitter_scraping.py"
-concat_csv="scripts/concat_csvs.py"
-check_complaint="scripts/check_complaints_llm.py"
+concat_csv_for_a_year="scripts/concat_csv_for_a_year.py"
+concat_year_wise_csvs="scripts/concat_year_wise_csvs.py"
+check_complaint="scripts/check_is_complaint_llm.py"
+categorize_complaints="scripts/check_complaints_llm.py"
 
 # Run each Python script with the specified year as an argument
 echo "Running scripts for year $year..."
 echo "Executing $twitter_scraping..."
 python3 "$twitter_scraping" "$year"
-echo "Executing $concat_csv..."
-python3 "$concat_csv"
+echo "Executing $concat_csv_for_a_year..."
+python3 "$concat_csv_for_a_year" "$year"
+echo "Executing $concat_year_wise_csvs..."
+python3 "$concat_year_wise_csvs" 
 echo "Executing $check_complaint..."
 python3 "$check_complaint"
+echo "Executing $categorize_complaints..."
+python3 "$categorize_complaints"
+echo ""
 
 echo "All scripts have been executed."
